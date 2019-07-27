7cbfcc2e2af1430c24694d419ea7d06148757941
Robert Šefman <robert@epodjetnik.com>
2019-07-27 21:44:08 +0200

    Stop input field jumping on focus

--- frontend/src/pages/vote.js
+++ frontend/src/pages/vote.js
@@ -1,14 +1,51 @@
 import React, { useEffect, useState } from "react"
 import { useApolloClient } from "react-apollo-hooks"
-import { CentralColumn } from "../components/styles"
+// import { CentralColumn } from "../components/styles"
 import { Form, Field } from "react-final-form"
-import { Button } from "rebass"
+import { Button, Heading, Text, Box } from "rebass"
+import styled from "styled-components"
+import { fontSize, lineHeight, fontFamily } from "styled-system"
 
 // import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
 import { WIDGET_VOTE_QUERY, SAVE_WIDGET_FEEDBACK_QUERY } from "../queries"
+
+const FullScreen = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  align-items: center;
+  min-height: 100vh;
+  text-align: center;
+`
+
+const InputBox = styled(Box)`
+  height: 60px;
+`
+
+const Input = styled("input")(
+  {
+    padding: "0.5rem 1rem",
+    width: "80%",
+    marginBottom: "14px",
+  },
+  fontSize,
+  lineHeight,
+  fontFamily
+)
+
+const ExplainerText = styled(Text)`
+  width: 80%;
+  text-align: left;
+  display: inline-block;
+  position: ralative;
+  top: -14px;
+`
+const BulletText = styled(Text)`
+  display: inline;
+`
 
 async function saveVote({ widgetId, voteType, apolloClient }) {
   const result = await apolloClient.mutate({
@@ -21,18 +58,42 @@ async function saveVote({ widgetId, voteType, apolloClient }) {
   })
 }
 
-function renderField({ id, label, type }) {
+const InputComponent = props => (
+  <InputBox>
+    <BulletText>{props.index + 1}. 👉</BulletText>
+    <Input
+      fontSize={[3, 4, 5]}
+      placeholder="Listen to your gut :)"
+      autoComplete="off"
+      {...props.input}
+    />
+    {props.meta.active ? (
+      <ExplainerText
+        fontSize={[0.5, 1, 1]}
+        style={{ textAlign: "left", color: "gray" }}
+      >
+        <strong>Enter</strong> to submit
+      </ExplainerText>
+    ) : null}
+  </InputBox>
+)
+
+function renderField({ index, id, label, type }) {
   return (
     <div key={id}>
-      <label>{label}</label>
+      <Heading fontSize={[3, 4, 5]}>
+        <label>{label}</label>
+      </Heading>
       <br />
-      <Field
-        name={`field_${id}`}
-        component="input"
-        type="text"
-        initialValue=""
-        placeholder="aa"
-      />
+      <Box>
+        <Field
+          name={`field_${id}`}
+          component={InputComponent}
+          type="text"
+          initialValue=""
+          index={index}
+        />
+      </Box>
     </div>
   )
 }
@@ -68,20 +129,21 @@ const VotePage = ({ pageContext }) => {
   }
 
   return (
-    <>
+    <FullScreen>
       <SEO title="Thank you" />
-      <CentralColumn style={{ paddingTop: "2em" }}>
-        <Form
-          onSubmit={onSubmit}
-          render={({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              {renderField(followupQuestions[fieldIndex])}
-              {/* <Button type="submit">Give feedback </Button> */}
-            </form>
-          )}
-        ></Form>
-      </CentralColumn>
-    </>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            {renderField({
+              index: fieldIndex,
+              ...followupQuestions[fieldIndex],
+            })}
+            {/* <Button type="submit">Give feedback </Button> */}
+          </form>
+        )}
+      ></Form>
+    </FullScreen>
   )
 }
 
