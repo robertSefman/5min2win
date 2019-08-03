77ebf9860bd06ff32c1e043eec9f7e929291ac9d
Robert Šefman <robert@epodjetnik.com>
2019-08-03 21:54:17 +0200

    3 inputs working

--- frontend/src/components/styles.js
+++ frontend/src/components/styles.js
@@ -1,38 +1,40 @@
 import styled from "styled-components"
-import { palette } from 'styled-tools'
+import { palette } from "styled-tools"
 
 export const CentralColumn = styled.div`
-    margin: 0 auto;
-    max-width: 960px;
+  margin: 0 auto;
+  max-width: 960px;
+`
 
-`;
-
-function headingSize({h1, h2, h3, h4, h5, h6}){
-    if (h1) {
-        return 0;
-    }else if (h2) {
-        return 1;
-    }else if (h3){
-        return 2;
-    }else if (h4){
-        return 3;
-    }else if (h5){
-        return 4;
-    }else{
-        return 0;
-    }
+function headingSize({ h1, h2, h3, h4, h5, h6 }) {
+  if (h1) {
+    return 0
+  } else if (h2) {
+    return 1
+  } else if (h3) {
+    return 2
+  } else if (h4) {
+    return 3
+  } else if (h5) {
+    return 4
+  } else {
+    return 0
+  }
 }
 
 // TO DO clean way to h1, h2, h3
 export const Heading = styled.h1`
-    font-size : ${props=>palette('headings', headingSize(props))};
-    line-height : ${props=>palette('headings', headingSize(props))};
-    margin-bottom : 0;
-
+  font-size: ${props => palette("headings", headingSize(props))};
+  line-height: ${props => palette("headings", headingSize(props))};
+  margin-bottom: 0;
 `
 
 export const Flex = styled.div`
-    display : flex;
-    flax-direction: ${props => ( props.column ? 'column' : 'row' )};
-    justify-content: center;
+  display: flex;
+  flax-direction: ${props => (props.column ? "column" : "row")};
+  justify-content: center;
+`
+
+export const Footer = styled.footer`
+  font-size: 0.5em;
 `
--- frontend/src/pages/vote.js
+++ frontend/src/pages/vote.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react"
 import { useApolloClient } from "react-apollo-hooks"
 import styled from "styled-components"
+import { Heading } from "rebass"
+import uuidv4 from "uuid/v4"
 
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 import { WIDGET_VOTE_QUERY, SAVE_WIDGET_FEEDBACK_QUERY } from "../queries"
 import { FullScreenForm } from "../components/FullScreenForm"
+import { Footer } from "../components/styles"
 
 const FullScreen = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr;
+  grid-template-rows: 140px 1fr;
   align-items: center;
   min-height: 100vh;
   text-align: center;
 `
 
-async function saveVote({ widgetId, voteType, apolloClient }) {
-  await apolloClient.mutate({
+async function saveVote({ userId, widgetId, voteType, apolloClient }) {
+  return await apolloClient.mutate({
     mutation: WIDGET_VOTE_QUERY,
     variables: {
+      userId: userId,
       widgetId: widgetId,
       thumbsup: voteType === "thumbsup",
       thumbsdown: voteType === "thumbsdown",
@@ -28,45 +32,88 @@ async function saveVote({ widgetId, voteType, apolloClient }) {
   })
 }
 
-// Final submission method
-//
-// const onSubmit = async ({ widgetId, values, apolloClient }) => {
-//   await apolloClient.mutate({
-//     mutation: SAVE_WIDGET_FEEDBACK_QUERY,
-//     variables: {
-//       widgetId,
-//       values: JSON.stringify(values),
-//     },
-//   })
-// }
+const VoteTypeHeading = ({ voteType, name }) =>
+  voteType === "thumbsup" ? (
+    <Heading fontSize={[5, 6, 7]}>👍 you enjoyed Swizec's {name} 👍</Heading>
+  ) : (
+    <Heading fontSize={[5, 6, 7]}>
+      👎 you didn't enjoy Swizec's {name} 👎
+    </Heading>
+  )
+const ThankYouView = () => (
+  <>
+    <div />
+    <div>
+      <img src="https://media.giphy.com/media/QAsBwSjx9zVKoGp9nr/giphy.gif" />
+      <Heading fontSize={[3, 4, 5]}>
+        ❤️️️ Thank you, you're the best! ❤️
+      </Heading>
+    </div>
+  </>
+)
+
+const FormView = ({ voteType, onSubmit, followupQuestions, name }) => (
+  <>
+    <VoteTypeHeading voteType={voteType} name={name} />
+    <FullScreenForm onSubmit={onSubmit} followupQuestions={followupQuestions} />
+  </>
+)
 
 const VotePage = ({ pageContext }) => {
   const apolloClient = useApolloClient()
-  const { widgetId, voteType, followupQuestions } = pageContext
-
-  const [fieldIndex, setFieldIndex] = useState(0)
+  const { userId, widgetId, voteType, followupQuestions, name } = pageContext
+  const [showThankYou, setShowThankYou] = useState(false)
+  const [voteId, setVoteId] = useState()
 
   useEffect(() => {
-    saveVote({ widgetId, voteType, apolloClient })
+    ;(async function() {
+      const result = await saveVote({
+        userId,
+        widgetId,
+        voteType,
+        apolloClient,
+      })
+      setVoteId(result.data.widgetVote.voteId)
+    })()
   }, [])
 
-  function onSubmit(values) {
-    if (fieldIndex >= followupQuestions.length - 1) {
-      console.log(values)
-    } else {
-      setFieldIndex(fieldIndex + 1)
+  async function onSubmit(answers) {
+    if (Object.values(answers).length >= followupQuestions.length) {
+      setShowThankYou(true)
     }
+
+    await apolloClient.mutate({
+      mutation: SAVE_WIDGET_FEEDBACK_QUERY,
+      variables: {
+        widgetId,
+        voteId,
+        voteType,
+        answers: JSON.stringify(answers),
+      },
+    })
   }
 
   return (
     <FullScreen>
       <SEO title="Thank You" />
-      <FullScreenForm
-        fieldIndex={fieldIndex}
-        followupQuestions={followupQuestions}
-      />
+      {showThankYou ? (
+        <ThankYouView />
+      ) : (
+        <FormView
+          voteType={voteType}
+          onSubmit={onSubmit}
+          followupQuestions={followupQuestions}
+          name={name}
+        />
+      )}
+      <Footer>
+        © {new Date().getFullYear()}, Built with ❤️ on the internet
+      </Footer>
     </FullScreen>
   )
 }
 
-export default VotePage
+export default ({ pageContext }) =>
+  pageContext.isCreatedByStatefulCreatePages ? null : (
+    <VotePage pageContext={pageContext} />
+  )
